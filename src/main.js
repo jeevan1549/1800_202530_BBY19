70512be520bf078167540b23ecd71d5441eccd59
Jeevan Jandu <jeevanjandu12@gmail.com>
2025-10-09 13:37:56 -0700

    updates

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
@@ -8,10 +7,3 @@ function sayHello() {
     
 }
 document.addEventListener('DOMContentLoaded', sayHello);
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-
-function sayHello() {}
-// document.addEventListener('DOMContentLoaded', sayHello);
->>>>>>> 97aabdb3f9aa9911ffc2221c1e63d5a27d70eb79
